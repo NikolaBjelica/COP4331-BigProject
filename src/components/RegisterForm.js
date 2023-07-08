58a0652308353ab6a88de773181acc3d0b80c291
Nikola Bjelica <100382766+NikolaBjelica@users.noreply.github.com>
2023-07-08 19:16:54 -0400

    Add files via upload

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,19 +1,64 @@
-import React from 'react'
-
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const RegisterForm = () => {
+    const router = useRouter();
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
 
-    const handleSubmit = (e) => {
+    const handleRegister = async (e) => {
         e.preventDefault()
-        // Handle form submission here
-}
+
+        if (confirmPassword !== password)
+        {
+            console.log("Passwords Do Not Match")
+        }
+
+        try 
+        {
+            const response = await fetch('/temp',
+            {
+                method: 'POST',
+                headers: 
+                {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password, firstName, lastName, email })
+            });
+      
+            if (response.ok) 
+            {
+                router.push(`/register/verification`);
+            } 
+
+            else if (response.status === 401) 
+            {
+                // Unauthorized: Invalid credentials
+                console.log('Invalid credentials');
+                // Display an error message to the user
+            } 
+            
+            else
+            {
+                // Other error occurred
+                console.log('An error occurred');
+                // Display a generic error message to the user
+            }
+          } 
+          catch (error) 
+          {
+                console.log('An error occurred', error);
+                // Handle other errors that may occur during the fetch request
+          }
+    }
   return (
     <div>
-        <form style={{ background: 'linear-gradient(125deg, rgba(236,229,199,1) 0%, rgba(205,194,174,1) 50%, rgba(168,157,135,1) 100%)' }} className="border-4 border-fourth py-10 px-4 space-y-4 rounded-3xl" onSubmit={handleSubmit}>
-            <div className = "text-center text-fourth text-3xl ">Sign Up</div>
+        <form style={{ background: 'linear-gradient(125deg, rgba(236,229,199,1) 0%, rgba(205,194,174,1) 50%, rgba(168,157,135,1) 100%)' }} className="border-4 border-fourth py-10 px-4 space-y-4 rounded-3xl" onSubmit={handleRegister}>
+            <div className = "text-center text-fourth text-5xl ">Sign Up</div>
             <div className="rounded-md shadow-sm -space-y-px text-fourth text-xl">
                 <div>
                     <label htmlFor="first-name">First Name</label>
@@ -23,8 +68,8 @@ const RegisterForm = () => {
                         type="text" 
                         required 
                         className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
+                        value={firstName}
+                        onChange={e => setFirstName(e.target.value)}
                     />
                 </div>
                 <div>
@@ -35,8 +80,8 @@ const RegisterForm = () => {
                         type="text" 
                         required                            
                         className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
+                        value={lastName}
+                        onChange={e => setLastName(e.target.value)}
                     />
                 </div>
                 <div>
@@ -59,8 +104,8 @@ const RegisterForm = () => {
                         type="email" 
                         required
                         className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>   
                 <div>
@@ -83,14 +128,14 @@ const RegisterForm = () => {
                         type="password" 
                         required
                         className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        value={confirmPassword}
+                        onChange={e => setConfirmPassword(e.target.value)}
                     />
                 </div>
             </div>
             <div>
                 <button type="submit"
-                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-fourth hover:bg-third focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-lg font-medium rounded-md text-white bg-fourth hover:bg-third focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Register
                 </button>
             </div>
@@ -98,7 +143,7 @@ const RegisterForm = () => {
                 <div className = "text-md text-center text-black">Already have an account?</div>
             </div>
             <div className = "flex items-center justify-center">
-            <a href="#" className="font-medium text-fourth hover:text-black">
+                    <a href="/login" className="font-medium text-fourth hover:text-black">
                         Login Here!
                     </a>
             </div>
