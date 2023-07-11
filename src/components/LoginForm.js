5484251ead3d2e8e62836160fa3709b52a7c2fd2
Nikola Bjelica <100382766+NikolaBjelica@users.noreply.github.com>
2023-07-11 17:25:32 -0400

    Add files via upload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,7 +20,9 @@ const LoginForm = () => {
                     {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({username, password})
+                    body: JSON.stringify({
+                        username: 'username', 
+                        password: 'password'})
                 });
 
                 if (response.ok)
